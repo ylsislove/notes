d1592e1e0eea1f4c3067e8eba912b84ed894ce3c
wangyu <916491013@qq.com>
2023-04-09 03:12:28 +0800

    feat: update website metadata and remove homepage hero image

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -9,11 +9,12 @@ const __dirname = getDirname(import.meta.url);
 export default defineUserConfig({
   base: "/notes/",
   lang: "zh-CN",
-  title: "你好， VuePress ！",
-  description: "这是我的第一个 VuePress 站点",
+  title: "学无止境",
+  description: "阿宇的学习笔记",
   head: [
     // 注入到当前页面的 HTML <head> 中的标签
     ["link", { rel: "icon", href: "/img/logo.png" }],
+    ["meta", { name: "keywords", content: "基础汉英类义词典,全栈学习笔记" }],
   ],
   theme: defaultTheme({
     logo: "/img/logo.png",
--- docs/README.md
+++ docs/README.md
@@ -1,6 +1,6 @@
 ---
-home: true
-heroImage: /img/logo.png
+home: false
+title: 笔记
 footer: MIT Licensed | Copyright © 2023 WangYu
 ---
 
