ae5c3e04e914b381c7816cfda592eff0868e5c16
wangyu <916491013@qq.com>
2023-04-11 05:20:19 +0800

    test

--- docs/.vuepress/config/navbar.ts
+++ docs/.vuepress/config/navbar.ts
@@ -1,4 +1,4 @@
 export default [
   { text: "我的博客", link: "https://blog.aayu.today" },
-  { text: "GitHub", link: "https://github.com/ylsislove" },
+  // { text: "GitHub", link: "https://github.com/ylsislove" },
 ];
